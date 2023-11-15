5a67e3aefcdbcf3b7fb2998f41c4f0ed4245383b
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-11-15 21:54:12 +0200

    React Routing

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,10 +3,14 @@ import * as request from '../lib/request.js';
 
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl);
-    return Object.values(result);
-    
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`
+    })
+    const result = await request.get(`${baseUrl}`);
+
+    //TODO: temp solution until migration to collection service
+    return Object.values(result).filter(comment => comment.gameId === gameId);
 };
 
 export const create = async (gameId, username, text) => {
